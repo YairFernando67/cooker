f5f68da6f5159409686b7252904f93cec3c92371
yairfacio <yair.facio11@gmail.com>
2021-01-10 19:33:09 -0600

    Change configuration

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
 
+  gem 'capistrano'
   gem 'capistrano-rvm',     require: false
   gem "capistrano-rails", "~> 1.5", require: false
   gem 'capistrano-bundler', '~> 1.6'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,42 +1,53 @@
-# Change these
-# server '67.205.174.87', port: 22, roles: [:web, :app, :db], primary: true
-server '138.197.110.143', port: 22, roles: [:web, :app, :db], primary: true
+# config valid only for current version of Capistrano
+lock '3.4.0'
+# config valid only for current version of Capistrano
+lock '3.4.0'
 
-set :repo_url,        'git@github.com:YairFernando67/cooker.git'
-set :application,     'cooker'
-set :user,            'mac_user'
-set :puma_threads,    [4, 16]
-set :puma_workers,    0
-set :puma_user, fetch(:user)
+set :application, 'cooker'
+set :user,            'admin_user'
+set :repo_url, 'git@github.com:YairFernando67/cooker.git'
 
-append :linked_files, "config/master.key"
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
-# Don't change these unless you know what you're doing
-set :pty,             true
-set :use_sudo,        false
-set :stage,           :production
-set :deploy_via,      :remote_cache
+# Default value for :scm is :git
+set :scm, :git
+
+# Default value for :format is :pretty
+set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+set :linked_files, fetch(:linked_files, []).push('.env', 'config/database.yml')
+
+# Default value for linked_dirs is []
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads', 'public/media')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+set :rails_env, 'production'
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+
+# puma
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_bind, "tcp://0.0.0.0:5000"
+set :puma_threads, [0, 5]
+set :puma_workers, 2
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-
-## Defaults:
-# set :scm,           :git
-# set :branch,        :master
-# set :format,        :pretty
-# set :log_level,     :debug
-# set :keep_releases, 5
-
-## Linked Files & Directories (Default None):
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :puma_init_active_record, true
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
@@ -46,21 +57,24 @@ namespace :puma do
       execute "mkdir #{shared_path}/tmp/pids -p"
     end
   end
+
+  before :start, :make_dirs
+  before :start, 'deploy:migrate'
+  before :restart, 'deploy:migrate'
 end
 
-namespace :deploy do
-  desc "Make sure local git is in sync with remote."
-  task :check_revision do
-    on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
-        puts "Run `git push` to sync changes."
-        exit
+desc 'Invoke a rake command on the remote server'
+task :invoke, [:command] => 'deploy:set_rails_env' do |task, args|
+  on primary(:app) do
+    within current_path do
+      with :rails_env => fetch(:rails_env) do
+        rake args[:command]
       end
     end
   end
+end
 
-  desc 'Initial Deploy'
+namespace :deploy do
   task :initial do
     on roles(:app) do
       before 'deploy:restart', 'puma:start'
@@ -68,10 +82,56 @@ namespace :deploy do
     end
   end
 
-  before :starting,     :check_revision
-  after  :finishing,    :cleanup
+  after :restart, :clear_cache do
+    invoke 'puma:restart'
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+    end
+  end
+
+  after :publishing, :restart
+  after :finishing, :cleanup
+  before :finishing, :restart
+  after :rollback, :restart
 end
 
-# ps aux | grep puma    # Get puma pid
-# kill -s SIGUSR2 pid   # Restart puma
-# kill -s SIGTERM pid   # Stop puma
+namespace :rails do
+  desc 'Open the rails console on each of the remote servers'
+  task console: 'rvm:hook' do
+    on roles(:app), primary: true do |host|
+      execute_interactively host, 'console production'
+    end
+  end
+end
+
+def execute_interactively(host, command)
+  command = "cd #{fetch(:deploy_to)}/current && #{SSHKit.config.command_map[:bundle]} exec rails #{command}"
+  puts command if fetch(:log_level) == :debug
+  exec "ssh -l #{host.user} #{host.hostname} -p #{host.port || 22} -t '#{command}'"
+end
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary server in each group
+# is considered to be the first unless any hosts have the primary
+# property set.  Don't declare `role :all`, it's a meta role.
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary server in each group
+# is considered to be the first unless any hosts have the primary
+# property set.  Don't declare `role :all`, it's a meta role.
+
+role :web, %w{admin_user@138.197.110.143}
+role :app, %w{admin_user@138.197.110.143}
+
+set :migration_role, :app
+
+set :rvm_ruby_version, '2.6.1@gemset-name'
+set :rvm_type, :user
+set :branch, :master
+
+# Extended Server Syntax
+# ======================
+# This can be used to drop a more detailed server definition into the
+# server list. The second argument is a, or duck-types, Hash and is
+# used to set extended properties on the server.
+
+server '138.197.110.143', user: 'admin_user', roles: %w{web app}
